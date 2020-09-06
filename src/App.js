5b290d1a440ee6176385c019268dd409d0f3c08b
Fei <fei-fei.zheng@ibm.com>
2020-09-06 17:00:33 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, setState] = useState({
+		monsters: [
+			{
+				name: 'Frankensteins',
+				id: 'asc1'
+			},
+			{
+				name: 'Dracula',
+				id: 'asr2'
+			},
+			{
+				name: 'Zombie',
+				id: 'as1w'
+			}
+		]
+	})
+
+	return (
+		<div className='App'>
+			{
+				state.monsters.map(monster => (
+					<h1 key={ monster.id }>{ monster.name }</h1>
+				))
+			}
+		</div>
+	);
 }
 
 export default App;
