4741a9afd4f8e3fec7ae576c1c208fd5362b4fd0
Fei <fei-fei.zheng@ibm.com>
2020-09-07 22:51:37 -0400

    Add search field

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import './scss/styles.scss';
 
-import { CardList } from './components/card-list/card-list';
+import { CardList } from './components/card-list';
 
 const App = () => {
-	const [state, setState] = useState({
-		monsters: []
-	});
+	const [monsters, setMonsters] = useState([]);
+	const [searchField, setSearchField] = useState('');
+
+	const filteredMonsters = () =>  monsters.filter(monster => 
+		monster.name.toLowerCase().includes(searchField.toLowerCase())
+	)
 
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(res => res.json())
-			.then(user => setState({ monsters: user }))
+			.then(user => setMonsters(user));
 	})
 
 	return (
 		<div className='App'>
-			<CardList monsters={state.monsters} />
+			<input 
+				type='search'
+				placeholder='search monster' 
+				onChange={e => setSearchField(e.target.value)}
+			/>
+			<CardList monsters={filteredMonsters()} />
 		</div>
 	);
 }
--- src/components/card-list.jsx
+++ src/components/card-list.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Card } from '../card/card';
+import { Card } from './card';
 
 export const CardList = props =>  {
 	return (
 		<div className='card-list'>
-			{ props.monsters.map(monster => 
-				<Card key={monster.id} monster={monster} />) 
+			{
+				props.monsters.map(monster => 
+					<Card key={monster.id} monster={monster} />) 
 			}
 		</div>
 	)
