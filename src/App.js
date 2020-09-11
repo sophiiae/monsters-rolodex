733a92d6e1c3a2c97f9fbac42542e019e9007bf1
Fei <fei-fei.zheng@ibm.com>
2020-09-11 14:28:54 -0400

    Add search box

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import {
+	CardList,
+	SearchBox
+} from './components/index';
+
 import './App.css';
 import './scss/styles.scss';
-
-import { CardList } from './components/card-list';
 
 const App = () => {
 	const [monsters, setMonsters] = useState([]);
 	const [searchField, setSearchField] = useState('');
 
-	const filteredMonsters = () =>  monsters.filter(monster => 
-		monster.name.toLowerCase().includes(searchField.toLowerCase())
-	)
-
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(res => res.json())
 			.then(user => setMonsters(user));
-	})
+	}, []);
+
+	const fileredResult = monsters.filter(monster => 
+		monster.name.toLowerCase().includes(searchField.toLowerCase())
+	)
 
 	return (
 		<div className='App'>
-			<input 
-				type='search'
+			<SearchBox 
 				placeholder='search monster' 
-				onChange={e => setSearchField(e.target.value)}
-			/>
-			<CardList monsters={filteredMonsters()} />
+				handleChange={e => setSearchField(e.target.value)}/>
+			<CardList monsters={fileredResult} />
 		</div>
 	);
 }
