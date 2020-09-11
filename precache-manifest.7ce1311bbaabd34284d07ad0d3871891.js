727d77e64c7d2b9c03ebad066a8f1fdc68474c16
Fei <fei-fei.zheng@ibm.com>
2020-09-11 15:59:05 -0400

    Updates

---
+++ precache-manifest.7ce1311bbaabd34284d07ad0d3871891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b8c1aac3961c15a384d4047ec0e9f8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d8c9c5c13135fc791e10",
+    "url": "/monsters-rolodex/static/css/main.dbd84a72.chunk.css"
+  },
+  {
+    "revision": "43cd1e4b3cd0a6e923d0",
+    "url": "/monsters-rolodex/static/js/2.51b5c0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.51b5c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c9c5c13135fc791e10",
+    "url": "/monsters-rolodex/static/js/main.c677896b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
